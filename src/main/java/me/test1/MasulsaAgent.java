f9185e12991014aef7c7b250923305f09a9ce23b
seokjin0414 <sars21@hanmail.net>
2023-08-08 11:40:00 +0900

    make javaagent

---
+++ src/main/java/me/test1/MasulsaAgent.java
@@ -0,0 +1,27 @@
+package me.test1;
+
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.matcher.ElementMatchers;
+import net.bytebuddy.utility.JavaModule;
+
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
+
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
+public class MasulsaAgent {
+    public static void premain(String agentAtgs, Instrumentation inst) {
+        new AgentBuilder.Default()
+                .type(ElementMatchers.any())
+                .transform(new AgentBuilder.Transformer() {
+                    @Override
+                    public DynamicType.Builder<?> transform(DynamicType.Builder<?> builder, TypeDescription typeDescription, ClassLoader classLoader, JavaModule javaModule, ProtectionDomain protectionDomain) {
+                        return builder.method(named("pullOut")).intercept(FixedValue.value("HOHOHOHOHO"));
+                    }
+                }).installOn(inst);
+
+    }
+}
